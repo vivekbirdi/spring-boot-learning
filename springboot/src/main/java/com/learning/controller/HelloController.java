bf0b704f1847436fcbaa390374e8e32c3a8939a2
vivekbirdi <vivekbirdi@gmail.com>
2023-02-07 03:02:20 +0530

    1. Commented swagger and jackson xml formatter dependencies in pom.xml. 2. i18n Example 	1. Added newYearGreeting method in HelloController. 	2. Added messages_{locale}.properties for locale messages.

--- springboot/src/main/java/com/learning/controller/HelloController.java
+++ springboot/src/main/java/com/learning/controller/HelloController.java
@@ -1,14 +1,30 @@
 package com.learning.controller;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 	
+	private MessageSource messageSource;
+	
+	public HelloController(MessageSource messageSource) {
+		this.messageSource =  messageSource;
+	}
+	
 	@GetMapping("/hello")
 	public String hello() {
 		return "Hello";
 	}
 
+	@GetMapping("/greetNewYear")
+	public String newYearGreeting() {
+		Locale locale =  LocaleContextHolder.getLocale();
+		return messageSource.getMessage("new.year.greeting", null, "Happy New Year All", locale);
+	
+	}
 }
