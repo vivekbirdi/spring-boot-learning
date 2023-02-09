4f1c1dbd820ec6e634bfa1a50cc200a71965ce99
vivekbirdi <vivekbirdi@gmail.com>
2023-02-09 12:05:00 +0530

    Spring-boot HATEOAS Example: - Added HATEOAS Starter dependency in pom.xml - Added Version-2 of getUser method for demo

--- springboot/src/main/java/com/learning/controller/UserController.java
+++ springboot/src/main/java/com/learning/controller/UserController.java
@@ -4,6 +4,8 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -40,15 +42,32 @@ public class UserController {
 		return userDao.findAll();
 	}
 	
+	
 	@GetMapping("/users/{userId}")
 	public User getUser(@PathVariable ("userId") int userId) {
 		User user = userDao.find(userId);
 		if(user ==null) {
 			throw new UserNotFoundException("Requested user does not exist");
-		}
-		
+		}		
 		return user;
 	}
+
+	/**
+	 * Get User version-2, for implementing HATEOAS
+	 * @param userId
+	 * @return
+	 */
+	@GetMapping("/v2/users/{userId}")
+	public EntityModel<User> getUserV2(@PathVariable ("userId") int userId) {
+		User user = userDao.find(userId);
+		if(user ==null) {
+			throw new UserNotFoundException("Requested user does not exist");
+		}		
+		EntityModel<User> userModel =  EntityModel.of(user);
+		WebMvcLinkBuilder link = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(getClass()).getUsers());
+		userModel.add(link.withRel("all_users"));
+		return userModel;
+	}	
 	
 	@PostMapping("/createUser")
 	public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
